d7bc290fa80cdfbb974f16dc31ee641172749ff6
Shekhar Kumar <shekharkumargupta@gmail.com>
2026-01-05 22:13:55 +0530

    prod yml added and username show is fixed

--- src/main/java/com/myco/users/controllers/AuthController.java
+++ src/main/java/com/myco/users/controllers/AuthController.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/v1/auth")
 @CrossOrigin(origins = "*")
